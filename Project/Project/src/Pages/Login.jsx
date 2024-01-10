68b7009bff62800b1b9f471ed5c17f4f5a95bf86
DinaEllinson <Dina@gmail.com>
2024-01-10 17:55:36 +0200

    start Login

---
+++ Project/Project/src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+ function Login(){
+    return(<><form>
+        <label>UserName</label>
+        <input placeholder="Enter UserName"></input>
+        <label>Password</label>
+        <input placeholder="Enter Pasword"></input>
+        </form></>) 
+  };
+  
+  export default Login;
