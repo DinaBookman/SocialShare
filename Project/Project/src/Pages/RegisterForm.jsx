57a6899914160e4d6e511465e1cb12d164c67d30
DinaEllinson <Dina@gmail.com>
2024-01-14 18:53:45 +0200

    POST newUser

--- Project/Project/src/Pages/RegisterForm.jsx
+++ Project/Project/src/Pages/RegisterForm.jsx
@@ -6,16 +6,40 @@ function RegisterForm(props){
     const {userName,password}= props;
     function confirmRegistration(event){
         event.preventDefault();
-        // let _ID=event.target[0].value;
-        // let _name=event.target[1].value;
         console.log(userName)
         console.log(password)
-
-        // let _email=event.target[2].value;
-        // let _address={
-            
-        }
-    
+        let newUser={
+            "id":event.target[0].value,
+            "name": event.target[1].value,
+            "username":  userName,
+            "email": event.target[2].value,
+            "address": {
+              "street": event.target[3].value,
+              "suite": event.target[4].value,
+              "city": event.target[5].value,
+              "zipcode": event.target[6].value,
+              "geo": {
+                "lat": event.target[7].value,
+                "lng": event.target[8].value
+              }
+            },
+            "phone": event.target[9].value,
+            "website":   password,
+            "company": {
+              "name": event.target[10].value,
+              "catchPhrase": event.target[11].value,
+              "bs": event.target[12].value
+            }
+          }
+        
+        const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newUser)
+            };
+        fetch('http://localhost:3000/users', requestOptions)
+                .then(response => response.json())      
+    }
 
     return(
         <form onSubmit={confirmRegistration}>
