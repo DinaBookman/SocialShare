47bb0cfb42996fdd4d38a8198e79b25b3af9af4d
אלינסון דינה < @B27>
2024-01-17 21:40:53 +0200

    dfgh

--- Project/Project/src/Pages/Posts/DisplayPost.jsx
+++ Project/Project/src/Pages/Posts/DisplayPost.jsx
@@ -1,15 +1,20 @@
 import React,{useState} from "react";
 import UpdatePost from "./UpdatePost";
 import DeletePost from "./DeletePost";
+import Comments from './Comments/Comments'
+import { useEffect } from "react";
 function DisplayPost(props){
     const {post,postsData,setPostsData}=props;
      const [showMore,setShowMore]=useState(false);
      const [showComments,setShowComments]=useState(false)
      const [updateTitle,setUpdatedTitle]=useState(post.title)
      const [updatedBody,setUpdatedBody]=useState(post.body)
-     const [inUpdated,setInUpdated]=useState(false);
+     const [inUpdate,setInUpdated]=useState(false);
     
-    
+    useEffect(()=>{
+    setUpdatedBody(post.body)
+    setUpdatedTitle(post.Title)},[inUpdate]
+    );
 return(<>
 <div style={showMore?{backGround:'#dcdcdc'}:{backGround:'white'}}></div>
     <td>{post.id}</td>
@@ -17,8 +22,8 @@ return(<>
     <td ><DeletePost post={post} postData={postsData} setPostsData={setPostsData} /></td>
    
     <UpdatePost post={post} postData={postsData} setPostsData={setPostsData}/>
-    {showMore&&<button}
-    {showMore&&showComments<Comments postId={post.id}/>}
+    {showMore&&<button style={{backgroundColor: showComments}}></button>}
+    {showMore&&showComments&&<Comments postId={post.id}/>}
     </>)
 }
 export default DisplayPost
