2552df036f06ff562d4bf3a8b2de56f466c9a558
Sara <mss054333@gmail.com>
2024-01-23 02:05:38 +0200

    add some style without home page

--- Project/Project/src/Pages/RegisterForm.jsx
+++ Project/Project/src/Pages/RegisterForm.jsx
@@ -1,5 +1,6 @@
  
 import { useNavigate } from "react-router-dom";
+import style from '../Pages/RegisterForm.module.css'
 
 function RegisterForm(props){
     const navigate= useNavigate();
@@ -47,39 +48,41 @@ function RegisterForm(props){
    
 
     return(
-        <form onSubmit={confirmRegistration}>
+      <>
+        <form className={style.registrationForm} onSubmit={confirmRegistration}>
         <label>ID</label>
-        <input type="number" placeholder="Enter Id" required></input><br/>
+        <input className={style.confirmRegInput} type="number" placeholder="Enter Id" required></input><br/>
         <label >Name</label>
-        <input type="text" placeholder="Enter name" required></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="Enter name" required></input><br/>
         <label >Email</label>
-        <input type="email" placeholder="Enter email" required></input><br/>
+        <input className={style.confirmRegInput} type="email" placeholder="Enter email" required></input><br/>
         <labal>Enter your address</labal><br/>
         <label>Street</label>
-        <input type="text" placeholder="Enter street" required></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="Enter street" required></input><br/>
         <label>suite</label>
-        <input type="text" placeholder="Enter suite" required></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="Enter suite" required></input><br/>
         <label>City</label>
-        <input type="text" placeholder="Enter city" required></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="Enter city" required></input><br/>
         <label>Zipcode</label>
-        <input type="text" placeholder="Enter zipcode" required></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="Enter zipcode" required></input><br/>
         <label>Geo</label><br/>
         <label>Lat</label>
-        <input type="number" placeholder="Enter lat" required></input><br/>
+        <input className={style.confirmRegInput} type="number" placeholder="Enter lat" required></input><br/>
         <label>Lng</label>
-        <input type="number" placeholder="Enter lng" required></input><br/>
+        <input className={style.confirmRegInput} type="number" placeholder="Enter lng" required></input><br/>
         <label>Phone number</label>
-        <input type="number" placeholder="Enter phonee number" required></input><br/>
+        <input className={style.confirmRegInput} type="number" placeholder="Enter phonee number" required></input><br/>
         <label>Enter the company details</label><br/>
         <label>Enter name</label>
-        <input type="text" placeholder="company's name"></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="company's name"></input><br/>
         <labal>Enter catchPhrase</labal>
-        <input type="text" placeholder="catchPhrase"></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="catchPhrase"></input><br/>
         <labal>Enter bs</labal>
-        <input type="text" placeholder="bs"></input><br/>
+        <input className={style.confirmRegInput} type="text" placeholder="bs"></input><br/>
 
-        <button type="submit">Confirm registration</button>
+        <button className={style.confirmRegBtn} type="submit">Confirm registration</button>
         </form>
+        </>
     )
 }
 
